591377d20f97810e9e23900ef084e857a91ebfb1
Louisl <louisl@synnex.com>
2019-03-08 17:28:30 +0800

    Initial commit

---
+++ src/main/java/com/marstalk/asyntaskdemo/AsynTaskDemoApplication.java
@@ -0,0 +1,13 @@
+package com.marstalk.asyntaskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynTaskDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynTaskDemoApplication.class, args);
+    }
+
+}
